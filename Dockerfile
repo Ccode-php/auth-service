46708936a78bae74def18aeedf144ab6c65da8a4
Ccode-php <ccodephp@gmail.com>
2026-01-28 03:26:36 +0500

    routes

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM php:8.2-fpm
 
-
-
-# System deps
 RUN apt-get update && apt-get install -y \
     git curl zip unzip libpng-dev libonig-dev libxml2-dev
 
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::post('/register', [AuthController::class,'register']);
+Route::post('/auth/register', [AuthController::class,'register']);
 Route::post('/login', [AuthController::class,'login']);
 Route::post('/refresh', [AuthController::class,'refresh']);
 
